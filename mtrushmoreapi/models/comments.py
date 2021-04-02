9c3e8f0c1584161f8293c4d204c8ce3dca247933
Your Github User Name <david.lav.williams@gmail.com>
2021-04-02 10:34:54 -0500

    the seed data is in

--- mtrushmoreapi/models/comments.py
+++ mtrushmoreapi/models/comments.py
@@ -5,4 +5,4 @@ class Comment(models.Model):
 
     rushmore_user = models.ForeignKey("Rushmore_User", on_delete=models.CASCADE)
     thread = models.ForeignKey("Thread", on_delete=models.CASCADE)
-    Comment = models.CharField(max_length=500)
+    comment = models.CharField(max_length=500)
