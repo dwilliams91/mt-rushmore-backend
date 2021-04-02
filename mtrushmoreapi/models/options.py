ef7d8e630c4220e88f8ee64754b6a277952a1d7d
Your Github User Name <david.lav.williams@gmail.com>
2021-04-02 10:12:58 -0500

    made all the file names for the fixtures

---
+++ mtrushmoreapi/models/options.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Option(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    option = models.CharField(max_length=250)
+    weight = models.models.IntegerField() 
