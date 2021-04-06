de430670f2f124c9d7fbda70dfb1f3e47fcbd5f0
Your Github User Name <david.lav.williams@gmail.com>
2021-04-06 11:27:31 -0500

    viewing a threads posts is coded but not working

--- mtrushmoreapi/views/option.py
+++ mtrushmoreapi/views/option.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from rest_framework import status
 from django.http import HttpResponseServerError
 from django.core.exceptions import ValidationError
-from mtrushmoreapi.models import Option, Thread, RushmoreUser
+from mtrushmoreapi.models import Option, Thread, RushmoreUser, Post
 from django.contrib.auth.models import User
 from rest_framework.decorators import action
 
@@ -21,9 +21,19 @@ class Options (ViewSet):
 
     @action(methods=['get'],detail=True)
     def viewThreadPosts(self,request,pk=None):
+        print("hi")
+
+        selected_thread=Thread.objects.get(pk=pk)
+        all_thread_post=Post.objects.get(thread_id=selected_thread)
+
+        all_options=[]
+        for item in all_thread_post:
+            option_to_add=Options.objects.filter(post_id=item)
+            all_options.append(option_to_add)
         
-
-
+        serializer=OptionSerializer(all_options, many=True, context={'request':request})
+        return Response(serializer.data)
+    
     # def create(self,request):
     #     thread=Thread()
     #     # authuser=User.objects.get(auth_token=request.auth)
