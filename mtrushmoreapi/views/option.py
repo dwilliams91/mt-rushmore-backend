12162c8e82e8f25cab41db8dcfb34b7df8b22a60
Your Github User Name <david.lav.williams@gmail.com>
2021-04-06 11:59:04 -0500

    bout to restart method and do it right

--- mtrushmoreapi/views/option.py
+++ mtrushmoreapi/views/option.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ValidationError
 from mtrushmoreapi.models import Option, Thread, RushmoreUser, Post
 from django.contrib.auth.models import User
 from rest_framework.decorators import action
+from django.forms.models import model_to_dict
 
 
 
@@ -29,10 +30,11 @@ class Options (ViewSet):
 
         all_options=[]
         for item in all_thread_post:
-            option_to_add=list(Option.objects.filter(post_id=item))
-            print(option_to_add[0]__dict__)
+            option_to_add=Option.objects.filter(post_id=item)
+            
+            
             all_options.append(option_to_add)
-        
+        print(all_options)
         serializer=OptionSerializer(all_options[1], many=True, context={'request':request})
 
         return Response(serializer.data)
