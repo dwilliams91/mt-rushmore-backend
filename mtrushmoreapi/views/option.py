64e3dff6929dadfaa97fe4f7e6046a2e40856e49
Your Github User Name <david.lav.williams@gmail.com>
2021-04-06 12:53:47 -0500

    trying the create method for options

--- mtrushmoreapi/views/option.py
+++ mtrushmoreapi/views/option.py
@@ -20,17 +20,47 @@ class Options (ViewSet):
         serializer=OptionSerializer(all_options, many=True, context={'request':request})
         return Response(serializer.data)
     
+    def create(self,request):
+        option=Option()
+        current_user=RushmoreUser.objects.get(user=request.auth.user)
+        post=Post()
+        thread=Thread.objects.get(id=request.data["thread"])
+        post.thread=thread
+        post.rushmore_user=current_user
+
+       
+        try:
+            post.save()
+            list_of_options=request.data["options"]
+            for item in list_of_options:
+                option.post=post
+                option.option=item
+                option.weight=1
+                option.save()
+
+
+
+            serializer=Thread(thread, many=False, context={'request':request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+        # {
+        #     token:asdfad
+        #     thread:1
+        #     option1: cheeseburgers,
+        #     option2:fries
+        #     option3: milkshake
+        #     option4:chocolate
+        # }
+        
+        
+        
 
     @action(methods=['get'],detail=True)
     def viewthreadposts(self,request,pk=None):
-        print("hello")
-
-        selected_thread=Thread.objects.get(pk=pk)
-        all_thread_post=Post.objects.filter(thread_id=selected_thread)
         
-
         all_options=Option.objects.filter(post_id__thread_id=pk)
-        print(all_options)
         
         serializer=OptionSerializer(all_options, many=True, context={'request':request})
 
